5682661d9ff42329b82cc2d4bd4331b15ea2bcae
Egon Patrick <egon.16@hotmail.com>
2019-01-14 21:30:11 -0200

    Configurando movimento das pecas

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -38,6 +38,15 @@ public class Board {
 		piece.position = position;
 	}
 	
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)) throw new BoardException("Posicao nao pertencente ao tabuleiro");
+		if (piece(position) == null)	return null;
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
+	
 	public boolean positionExists (int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
 	}
@@ -49,5 +58,7 @@ public class Board {
 		if(!positionExists(position)) throw new BoardException("Posicao nao pertencente ao tabuleiro");
 		return piece(position)!= null;
 	}
+	
+	
 	
 }
